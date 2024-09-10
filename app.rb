2ff57bebb576d4660fc01bb73dab6377c33bab03
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-10 01:36:30 +0000

    roche et papier et ciseaux

--- app.rb
+++ app.rb
@@ -7,3 +7,27 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  erb(:rock, {:layout => :layout})
+
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  erb(:paper, {:layout => :layout})
+
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  erb(:scissors, {:layout => :layout})
+
+end
